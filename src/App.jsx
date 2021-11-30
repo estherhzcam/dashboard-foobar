f93093b053421c85def8f2a692fe4770d98ccf7f
Marta Skrzypczak <mart51w7@stud.kea.dk>
2021-11-30 14:13:52 +0100

    update fetch

--- src/App.jsx
+++ src/App.jsx
@@ -9,32 +9,32 @@ import { SoldBeers } from "./components/SoldBeers.jsx";
 import { Queue } from "./components/Queue.jsx";
 
 function App() {
-  
   const [count, setCount] = useState(0);
-  const [articles, setArticles] = useState([]);  
+  const [articles, setArticles] = useState([]);
   useEffect(() => {
-    async function fetchData() {
-      const res = await fetch("https://foo-bar-project.herokuapp.com/");
-      const data = await res.json();
-      setArticles(data);
-      console.log("data", data);
-    }
-    fetchData();
+    const interval = setInterval(() => {
+      async function fetchData() {
+        const res = await fetch("https://foo-bar-project.herokuapp.com/");
+        const data = await res.json();
+        setArticles(data);
+        console.log("data", data);
+      }
+      fetchData();
+    }, 5000);
   }, []);
   return (
     <div className="App">
       <Menu articles={articles} />
       <section id="cards-wrapper">
-      <PendingOrders articles={articles} />
-      <Bartenders articles={articles} />
-      <Levels articles={articles} />
-      <Barrels articles={articles.storage} />
-      <SoldBeers articles={articles} />
-      <Queue articles={articles} />
+        <PendingOrders articles={articles} />
+        <Bartenders articles={articles} />
+        <Levels articles={articles} />
+        <Barrels articles={articles.storage} />
+        <SoldBeers articles={articles} />
+        <Queue articles={articles} />
       </section>
     </div>
   );
-
 }
 
 export default App;
