103f42a539df301e3d6a787269362e9481d20965
Esther <esth2577@stud.kea.dk>
2021-11-30 15:34:03 +0100

    time until closure added

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,4 +1,13 @@
 export function Menu(props){
+    let currentTime = props.articles.timestamp;
+    let myDate = new Date(currentTime);
+    let myHour = myDate.getHours();
+    let myMinutes = myDate.getMinutes();
+    let remainingMinutes = 60-myMinutes
+    let remainingHours = 22 - 1 - myHour;
+   
+    //console.log(remainingMinutes)    
+
     return(
         <section className="top_menu">
             <div className="logo">
@@ -7,11 +16,11 @@ export function Menu(props){
             <section className="menucardwrapper">
             <div className="menucard" id="time">
                 <h1>Time until closure</h1>
-                <h1 id="remaining_time">2:00</h1>
+                <h1 id="remaining_time">{myMinutes > 0 ? remainingHours : myHour}:{myMinutes > 0 ? remainingMinutes : "00"}</h1>
             </div>
             <div className="menucard" id="people">
             <h1>People in queue</h1>
-                <h1 id="people_queue">2:00</h1>
+                <h1 id="people_queue">13</h1>
             </div>
             <div className="menucard" id="sales">
             <h1>Sales</h1>
