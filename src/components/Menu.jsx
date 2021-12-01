09217d71775ed20cec6c6eaab416e408966f779c
Esther <esth2577@stud.kea.dk>
2021-12-01 19:24:09 +0100

    time until closure calculation recovered

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,13 +1,23 @@
 export function Menu(props){
-  return (
-    <section className="top_menu">
-      <div className="logo">
-        <h1>FooBar</h1>
-      </div>
-      <section className="menucardwrapper">
-        <div className="menucard" id="time">
-          <h1>Time until closure</h1>
-          <h1 id="remaining_time">2:00</h1>
+    let currentTime = props.articles.timestamp;
+    let myDate = new Date(currentTime);
+    let myHour = myDate.getHours();
+    let myMinutes = myDate.getMinutes();
+    let remainingMinutes = 60 - myMinutes
+    let remainingHours = 22 - 1- myHour;
+    let oclockHours = 22 - myHour
+   
+    //console.log(remainingMinutes)    
+
+    return(
+        <section className="top_menu">
+            <div className="logo">
+                <h1>FooBar</h1>
+            </div>
+            <section className="menucardwrapper">
+            <div className="menucard" id="time">
+                <h1>Time until closure</h1>
+                <h1 id="remaining_time">{myMinutes > 0 ? remainingHours : oclockHours}:{myMinutes > 0 ? remainingMinutes : "00"}</h1>
         </div>
         <div className="menucard" id="people">
           <h1>People in queue</h1>
