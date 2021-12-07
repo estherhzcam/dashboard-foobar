09f4a6f99c1225ca24ba174c6401a19060e882ff
Esther <esth2577@stud.kea.dk>
2021-12-07 09:39:13 +0100

    orders being filtered to diplay max 4

--- src/components/Order.jsx
+++ src/components/Order.jsx
@@ -1,17 +1,9 @@
-
 export function Order(props) {
   //we need to find if an element is duplicated
-  
+
   const listProducts = props.order;
   const copyOfProducts = [...listProducts];
-  //const [product, setAmount] = useState([]);
-  /* const copyProducts =  listProducts.filter(function(item, pos, self){
-    return self.indexOf(item) ==pos;
-  }); 
-  console.log("copy is "+copyProducts)
-  if(listProducts.length > copyProducts.length){
 
-  }*/
   const newArray = listProducts.map((product) => {
     let counter = 0;
     copyOfProducts.map((copy) => {
@@ -26,38 +18,16 @@ export function Order(props) {
     return { beerName: product, amount: counter };
   });
 
-  console.log(typeof newArray, newArray);
-  const filteredProductList = newArray.filter((elm, index, self)=> {
-    return(  
-    index === self.findIndex((e)=> (e.beerName === elm.beerName && e.amount === elm.amount)))
-  })
-  /* const filteredProductList =  newArray.filter(function(item, pos, self){
-    return self.indexOf(item) === pos;})
- */
-    console.log(filteredProductList);
-/* const filteredProductList = newArray.filter((elm, index, self)=> {
-  
-    index === self.findIndex((e)=> (e.beerName === elm.beerName && e.amount === elm.amount))
-  }) */ 
+  const filteredProductList = newArray.filter((elm, index, self) => {
+    return index === self.findIndex((e) => e.beerName === elm.beerName && e.amount === elm.amount);
+  });
 
-
-   
-  /* Array.from(new Set(newArray.map((beer)=>{
-    beer.beerName
-  }))).map(name=>{return newArray.find(beer=>beer.beerName === name)}) */
-
-
- 
-
-  /* const orderProducts = props.order.map((product, index) => {
-    return <li key={index}>{product}</li>;
-  }); */
-
-  const orderProducts = filteredProductList.map((product, index) =>{
-    return <li key={index}>{product.beerName}</li>
-  })
-  const amountProducts = filteredProductList.map((product, index) =>{
-    return <li key={index}>{product.amount}</li>})
+  const orderProducts = filteredProductList.map((product, index) => {
+    return <li key={index}>{product.beerName}</li>;
+  });
+  const amountProducts = filteredProductList.map((product, index) => {
+    return <li key={index}>{product.amount}</li>;
+  });
 
   return (
     <tr>
--- src/components/PendingOrders.jsx
+++ src/components/PendingOrders.jsx
@@ -2,10 +2,13 @@ import { Order } from "./Order.jsx";
 export function PendingOrders(props) {
   //let newOrder;
   const queue = props.articles.queue;
+  console.log("queue", queue.length)
+  //Deoending on the length of the queue we'll display diferent content. Must be cleaned
 
   const newOrder = queue.map((order) => <Order key={order.id} {...order} />);
+  const slicedOrders = queue.slice(0,4).map((order) => <Order key={order.id} {...order} />)
 
-  if (queue.length > 0)
+  if (queue.length > 0 && queue.length <= 4)
     return (
       <section className="card">
         <div className="header">
@@ -27,11 +30,14 @@ export function PendingOrders(props) {
               <th>Quantity</th>
             </tr>
           </thead>
-          <tbody>{newOrder}</tbody>
+          <tbody style={{
+            gap: "1em"
+          
+          }}>{newOrder}</tbody>
         </table>
       </section>
     );
-  else
+  else if(queue.length === 0){
     return (
       <section className="card">
         <div className="header">
@@ -52,18 +58,37 @@ export function PendingOrders(props) {
           <tbody></tbody>
         </table>
       </section>
-    );
+    );} else if(queue.length > 4){
+      //filter the queue here to avoid displaying more than 4 orders
+     
+      return(
+        <section className="card">
+        <div className="header">
+          <h1>Pending orders</h1>
+        </div>
+        <table
+          style={{
+            width: "100%",
+            textAlign: "left",
+            gap: "1em",
+            fontSize: "0.9em",
+            marginLeft: "0",
+          }}
+        >
+          <thead>
+            <tr>
+              <th>Order Nº</th>
+              <th>Products</th>
+              <th>Quantity</th>
+            </tr>
+          </thead>
+          <tbody style={{
+                  gap: "1em",
+                }}>{slicedOrders}</tbody>
+        </table>
+      </section>
+      )
+    }
 }
 
-/* 
-function GetProducts(order){
-return(
-  <td className="products">
-  <ul>
-    <li>Row 26</li>
-    <li>Row 26</li>
-    <li>Row 26</li>
-  </ul>
-</td>
-)
-} */
+
