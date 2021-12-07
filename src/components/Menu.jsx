949e972c873c5a7f5abe30e5ea39b0322b222ffa
Esther <esth2577@stud.kea.dk>
2021-12-07 09:46:12 +0100

    time until closure negative

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -10,7 +10,7 @@ export function Menu(props) {
   const closingTime = closingDate.getTime();
   const remainingTime = closingTime - currentTime;
   const timeUntilClosure = hourFromMs(remainingTime);
-  console.log(remainingTime);
+ 
 
   return (
     <section className="top_menu">
@@ -20,7 +20,7 @@ export function Menu(props) {
       <section className="menucardwrapper">
         <div className="menucard" id="time">
           <h1>Time until closure</h1>
-          {<h1 id="remaining_time">{timeUntilClosure}</h1>}
+          {<h1 id="remaining_time">{remainingTime > 0 ? timeUntilClosure : "00:00"}</h1>}
         </div>
         <div className="menucard" id="people">
           <h1>People in queue</h1>
